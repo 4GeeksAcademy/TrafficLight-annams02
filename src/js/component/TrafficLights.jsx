8044ac50944051fd63bb5920a722687da5010994
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-04-19 18:29:58 +0000

    glow finalizado

---
+++ src/js/component/TrafficLights.jsx
@@ -0,0 +1,34 @@
+
+import React , {useState, useEffect}from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+
+
+const TrafficLights = () => {
+	const stages = {
+		stop: { red: true, amber: false, green: false, next: "readyToGo" },
+		readyToGo: { red: true, amber: true, green: false, next: "go" },
+		go: { red: false, amber: false, green: true, next: "readyToStop" },
+		readyToStop: { red: false, amber: true, green: false, next: "stop" }
+	};
+
+	const [stage, setStage] = useState(stages.stop);
+
+	useEffect(() => console.log(stage), [stage]);
+
+	return	<Display {...stage} onClick={() => setStage(stages[stage.next])} />;
+	};
+
+	const Display = ({ red, amber, green, onClick }) => (
+	<div className="black-box" onClick={onClick}>
+		<div className={`light ${red ? "red" : "inacti"}`} />
+		<div className={`light ${amber ? "amber" : "inactive"}`} />
+		<div className={`light ${green ? "green" : "inactive"}`} />
+	</div>
+	);
+
+	
+export default TrafficLights;
